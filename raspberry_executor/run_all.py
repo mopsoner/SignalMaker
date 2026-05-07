5ee0cb9a2da02440546e1766a710986f807e6859
mopsoner <mopsoner@gmail.com>
2026-05-07 13:11:46 -0400

    Use Raspberry 360 dashboard UI

--- raspberry_executor/run_all.py
+++ raspberry_executor/run_all.py
@@ -6,7 +6,7 @@ from raspberry_executor.config import load_settings
 from raspberry_executor.env_store import ensure_env
 from raspberry_executor.logging_setup import setup_logging
 from raspberry_executor.main_spot_oco import main as executor_main
-from raspberry_executor.web_local import run_web
+from raspberry_executor.web_dashboard import run_web
 
 logger = setup_logging("raspberry-executor")
 
@@ -19,7 +19,7 @@ def main() -> None:
 
     web_thread = threading.Thread(target=run_web, kwargs={"host": host, "port": port}, daemon=True)
     web_thread.start()
-    logger.info("local web UI started http://%s:%s", host, port)
+    logger.info("local 360 dashboard started http://%s:%s", host, port)
 
     candle_thread = threading.Thread(target=candle_feed_loop, daemon=True)
     candle_thread.start()
