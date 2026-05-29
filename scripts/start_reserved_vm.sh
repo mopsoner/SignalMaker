f5167c2d6c413e89c56c54cb89ad13b328523e8e
mopsoner <mopsoner@gmail.com>
2026-05-28 20:54:53 -0400

    Start momentum engine worker in reserved VM script

--- scripts/start_reserved_vm.sh
+++ scripts/start_reserved_vm.sh
@@ -18,11 +18,14 @@ EXECUTOR_PID=$!
 bash scripts/start_scheduler_worker.sh &
 SCHEDULER_PID=$!
 
+bash scripts/start_momentum_engine_worker.sh &
+MOMENTUM_ENGINE_PID=$!
+
 FRONTEND_PORT=${FRONTEND_PORT:-5000} bash scripts/start_frontend.sh &
 FRONTEND_PID=$!
 
 cleanup() {
-  kill "$API_PID" "$PIPELINE_PID" "$EXECUTOR_PID" "$SCHEDULER_PID" "$FRONTEND_PID" 2>/dev/null || true
+  kill "$API_PID" "$PIPELINE_PID" "$EXECUTOR_PID" "$SCHEDULER_PID" "$MOMENTUM_ENGINE_PID" "$FRONTEND_PID" 2>/dev/null || true
 }
 
 trap cleanup EXIT INT TERM
